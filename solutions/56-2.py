052af49f610d942f36f662d9ebb6ad7737a16b41
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-06-17 01:30:12 +0000

    Add more efficient solution for Problem #56

--- solutions/56-2.py
+++ solutions/56-2.py
@@ -1,13 +1,13 @@
 """
-Simple solution for Algorithms #56: Merge Intervals
+More efficient solution for Algorithms #56: Merge Intervals
 
 - N: Number of intervals
 - Space Complexity: O(N)
   - Assumes that we can modify given `intervals` list.
-- Time Complexity: O(N^2)
-  - Compares every pair once
+- Time Complexity: O(N log N)
+  - Sorting intervals takes O(N log N)
 
-Runtime: 64 ms, faster than 32.46% of Python3 online submissions for Merge Intervals.
+Runtime: 52 ms, faster than 71.88% of Python3 online submissions for Merge Intervals.
 Memory Usage: 14.1 MB, less than 97.66% of Python3 online submissions for Merge Intervals.
 """
 class Solution:
@@ -20,12 +20,14 @@ class Solution:
             return None
 
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        for i in range(len(intervals)):
-            for j in range(i+1, len(intervals)):
-                new_interval = self.merge_two_intervals(intervals[i], intervals[j])
-                if new_interval is not None:
-                    intervals[i] = None
-                    intervals[j] = new_interval
-                    break
+        # Assume we can modify intervals in-place
+        # Sorts lexicographically
+        intervals.sort()
+
+        for i in range(len(intervals)-1):
+            new_interval = self.merge_two_intervals(intervals[i], intervals[i+1])
+            if new_interval is not None:
+                intervals[i] = None
+                intervals[i+1] = new_interval
 
         return [interval for interval in intervals if interval is not None]
