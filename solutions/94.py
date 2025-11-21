55c72c900af59380976e8061b69688b8ef97baaa
Ryan Lee <seungjaeryanlee@gmail.com>
2025-11-21 00:48:36 +0000

    Add LeetCode #94

---
+++ solutions/94.py
@@ -0,0 +1,22 @@
+from typing import List, Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+
+        output = []
+        output.extend(self.inorderTraversal(root.left))
+        output.append(root.val)
+        output.extend(self.inorderTraversal(root.right))
+
+        return output
